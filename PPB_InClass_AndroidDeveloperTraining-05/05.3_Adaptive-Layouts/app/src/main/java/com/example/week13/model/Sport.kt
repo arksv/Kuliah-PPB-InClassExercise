80b11dd953acaecf54c93108b57e2aa39ce8b82c
arksv <72218011+arksv@users.noreply.github.com>
2023-06-18 14:59:33 +0700

    Lesson 05.3_Adaptive-Layouts

---
+++ PPB_InClass_AndroidDeveloperTraining-05/05.3_Adaptive-Layouts/app/src/main/java/com/example/week13/model/Sport.kt
@@ -0,0 +1,12 @@
+package com.example.week13.model
+
+import com.example.week13.R
+
+data class Sport(
+    val id: Int,
+    val titleResourceId: Int,
+    val subTitleResourceId: Int,
+    val imageResourceId: Int,
+    val sportsImageBanner: Int,
+    val newsDetails: Int = R.string.sports_news_detail_text
+)
